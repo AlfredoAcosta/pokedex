fb4a0cba9e92be01462bfea91032d19cfc6491c6
JaimeGuza <jimm.guza@gmail.com>
2021-10-05 23:12:23 -0500

    Se agrego otro fetch en el Sprites. Ya imprime

--- src/Characters.js
+++ src/Characters.js
@@ -1,22 +1,44 @@
 import React, {useState, useEffect} from "react";
 
+// Components
+import { Sprites } from './Sprites';
+
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
 
   useEffect(()=>{
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=150&offset=200')
-    .then(response => response.json())
-    .then(data => setCharacters(data.results))
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=150&offset=0')
+    //fetch('https://pokeapi.co/api/v2/pokemon/12')
+      .then(response => response.json())
+      .then(data => setCharacters(data.results))
   },[])
+
+  console.log('caracter: ', characters)
 
   return(
     <div>
       {characters.map(character => (
-        <h2>{character.name}</h2>
+        <Sprites pokemonName={character.name} />
       ))}
-      <h2>{}</h2>
     </div>
   )
+
+  /*return (
+    <div className="App">
+      <section className="App-content">
+        {
+          gifs.map(gif => {
+            return <div>
+              <h4>{gif.title}</h4>
+              <small>{gif.id}</small>
+              <img alt={gif.title} scr={gif.url} />
+            </div>
+          })
+        }
+      </section>
+    </div>
+  );*/
+
 }
 
 export default Characters;
